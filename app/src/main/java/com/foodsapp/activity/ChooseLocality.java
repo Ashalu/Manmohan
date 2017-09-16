5a851c475ef6238c9c0e1b5fcb81c202b8a20277
Aashish Kumar <akbrother02@gmail.com>
2017-09-16 18:59:34 +0530

    first commit

---
+++ app/src/main/java/com/foodsapp/activity/ChooseLocality.java
@@ -0,0 +1,15 @@
+package com.foodsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.foodsapp.R;
+
+public class ChooseLocality extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_locality);
+    }
+}
